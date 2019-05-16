9d3cc0ead5065fc330414553acfbc2ec1871df6d
Devin Arnold <dbarnold321@gmail.com>
2019-05-16 17:46:26 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
     i = 0 
     result =[]
     while i < collection.length
-   result << yield(collection[i])
+yield(collection[i])
     #if i == true
      # result.push(i)
     #else 
