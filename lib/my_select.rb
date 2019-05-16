47ec688e82857de45b095e2a5e09964aa476e940
Devin Arnold <dbarnold321@gmail.com>
2019-05-16 17:53:19 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,14 +1,11 @@
 def my_select(collection)
     i = 0 
     result =[]
-    last = []
     while i < collection.length
 result << yield(collection[i])
+
     i= i+ 1
 end
- if result == true 
- last << (collection[i])
+ result
  end
- "false"
- last
- end
+ 
