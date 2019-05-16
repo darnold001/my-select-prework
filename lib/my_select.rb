92b297338a378b18823ec28f1d9bba53aadaacd8
Devin Arnold <dbarnold321@gmail.com>
2019-05-16 17:40:50 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,11 +2,12 @@ def my_select(collection)
     i = 0 
     result =[]
     while i < collection.length
-   result << yield(collection[i])
-    #if i == true
-     # result.push(i)
-    #else 
-     # "false"
+   result << yield(decision[i])
+    if i == true
+      decision.push(i)
+    else 
+      "false"
+    end
     i= i+ 1
   
 end
